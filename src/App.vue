5d94f54349a663f05b5c81bd22899f0f1a69cf09
Verônica Helena <desenvolveronica@gmail.com>
2022-09-28 08:49:59 -0300

    Projeto Base

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Teste</div>
+</template>
+
+<script>
+  export default {
+  components: {  },
+    data: () => ({ 
+      drawer: null,
+      items: [
+          { title: 'Tarefas', icon: 'mdi-view-dashboard', to:'/' },
+          { title: 'Sobre', icon: 'mdi-help-box', to:'/sobre'  },
+        ],
+    }),
+  }
+</script>
