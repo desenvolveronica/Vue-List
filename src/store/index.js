0a2714f8f88f3a8c3c53b9157b9690c4880cb299
Verônica Helena <desenvolveronica@gmail.com>
2022-10-01 16:06:56 -0300

    Ajuste

--- src/store/index.js
+++ src/store/index.js
@@ -1,36 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import state from './state.js'
+import mutations from './mutations.js'
+import actions from './actions.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state:{
-    tarefas:[
-      {id: 1, titulo: 'Estudar Vue', concluido: false},
-      {id: 2, titulo: 'Tomar remédio', concluido: false}
-    ]
-  },
-
-  mutations:{
-    adicionaTarefa(state, titulo){
-      if(titulo){
-        state.tarefas.push({
-          id: new Date().getTime(),
-          titulo,
-          concluido: false
-        })
-    }
-    console.log('adiciona tarefa');
-    }, 
-
-    removeTarefa(state, id){
-      state.tarefas = state.tarefas.filter(tarefa => tarefa.id !== id)
-    }
-  },
-
-  actions:{
-    
-  },
+  state,
+  mutations,
+  actions,
   modules: {
   }
 })
